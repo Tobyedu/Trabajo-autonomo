31ed823a365af5a10ffce3f3d3737d750431adac
Victorjrtoby40 <58490764+Victorjrtoby40@users.noreply.github.com>
2019-12-03 17:16:23 -0500

    Add files via upload

---
+++ Alimentos.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProGrupalSystemVentaComiRapida
+{
+    public class Alimentos
+    {
+        public int Id
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int DescripcionComidaRapida
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int DescripcionBebidas
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int CantidadDeComidaRapida
+        {
+            get => default;
+            set
+            {
+            }
+        }
+
+        public int CantidadDeBebidas
+        {
+            get => default;
+            set
+            {
+            }
+        }
+    }
+}
